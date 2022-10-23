6d085489309704c8920c5a672fb83cd3f57458d7
evgeniytikhonenko <tihonenko87@gmail.com>
2022-10-23 17:09:10 +0700

    dependency injection with setters

--- src/main/java/com/example/spring2/introduction/Person.java
+++ src/main/java/com/example/spring2/introduction/Person.java
@@ -1,11 +1,19 @@
 package com.example.spring2.introduction;
 
 public class Person {
-    private final Pet pet;
+    private Pet pet;
 
     public Person(Pet pet) {
         this.pet = pet;
         System.out.println("Person bean is created");
+    }
+
+    public Person() {
+        System.out.println("Person bean is created");
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
     }
 
     public void callPet() {
