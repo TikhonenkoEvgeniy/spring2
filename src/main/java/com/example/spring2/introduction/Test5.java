e0e2783722d7207e4572749ab163d5c20dc05e26
evgeniytikhonenko <tihonenko87@gmail.com>
2022-10-24 15:01:33 +0700

    init & destroy methods

---
+++ src/main/java/com/example/spring2/introduction/Test5.java
@@ -0,0 +1,19 @@
+package com.example.spring2.introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test5 {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context2.xml");
+
+        Dog dog = context.getBean("dog", Dog.class);
+        dog.say();
+
+        Dog dog2 = context.getBean("dog", Dog.class);
+        dog2.say();
+
+        System.out.println("Pre-destroy");
+        context.close();
+    }
+}
