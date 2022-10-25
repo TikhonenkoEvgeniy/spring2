dd2180b4ade025539f84803f2c591cfc0ff4170e
evgeniytikhonenko <tihonenko87@gmail.com>
2022-10-25 10:22:41 +0700

    autowired with constructor

--- src/main/java/com/example/spring2/introduction/ConfigWithAnnotations1.java
+++ src/main/java/com/example/spring2/introduction/ConfigWithAnnotations1.java
@@ -7,6 +7,9 @@ public class ConfigWithAnnotations1 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context3.xml");
 
+        Person person = context.getBean("person", Person.class);
+        person.callPet();
+
         Cat myCat = context.getBean("cat", Cat.class);
         myCat.say();
         context.close();
--- src/main/java/com/example/spring2/introduction/Person.java
+++ src/main/java/com/example/spring2/introduction/Person.java
@@ -1,10 +1,15 @@
 package com.example.spring2.introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Person {
     private Pet pet;
     private String name;
     private int age;
 
+    @Autowired
     public Person(Pet pet) {
         this.pet = pet;
         System.out.println("Person bean is created");
