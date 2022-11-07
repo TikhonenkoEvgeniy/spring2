71f92a545383424375a97325bf95ae41cb584b20
evgeniytikhonenko <tihonenko87@gmail.com>
2022-11-07 16:12:21 +0700

    one-to-one relation4

---
+++ src/main/java/com/example/spring2/hibernate2/Test3.java
@@ -0,0 +1,33 @@
+package com.example.spring2.hibernate2;
+
+import com.example.spring2.hibernate2.model.Detail;
+import com.example.spring2.hibernate2.model.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test3 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure()
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+        Session session = null;
+
+        try {
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            Detail detail = session.get(Detail.class, 4);
+            System.out.println(detail.getEmployee());
+            session.delete(detail);
+
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } finally {
+            if (session != null) { session.close(); }
+            if (factory != null) { factory.close(); }
+        }
+    }
+}
