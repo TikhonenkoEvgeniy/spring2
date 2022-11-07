bfc262dcb09c8adef9d75ed6b604a8e0d68c1ded
evgeniytikhonenko <tihonenko87@gmail.com>
2022-11-07 15:46:21 +0700

    one-to-one relation2

--- src/main/java/com/example/spring2/hibernate2/Test1.java
+++ src/main/java/com/example/spring2/hibernate2/Test1.java
@@ -1,6 +1,7 @@
 package com.example.spring2.hibernate2;
 
-import com.example.spring2.hibernate.model.Employee;
+import com.example.spring2.hibernate2.model.Detail;
+import com.example.spring2.hibernate2.model.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -10,17 +11,42 @@ public class Test1 {
         SessionFactory factory = new Configuration()
                 .configure()
                 .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
                 .buildSessionFactory();
+        Session session = null;
 
         try {
-            Session session = factory.getCurrentSession();
-            Employee employee = new Employee("Alexandr", "Petrov", "IT", 600);
+//            Session session = factory.getCurrentSession();
+//
+//            Employee employee = new Employee("Evgeniy", "Tikhonenko", "IT", 500);
+//            Detail detail = new Detail("Omsk", "+79620555222", "tihonenko87@gmail.com");
+//            employee.setEmpDetail(detail);
+//
+//            session.beginTransaction();
+//            session.save(employee);
+//            session.getTransaction().commit();
+//            System.out.println("Done");
+
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+//
+//            Employee employee = session.get(Employee.class, 10);
+//            System.out.println(employee.getEmpDetail());
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done");
+
+            session = factory.getCurrentSession();
             session.beginTransaction();
-            session.save(employee);
+
+            Employee employee = session.get(Employee.class, 2);
+            session.delete(employee);
+
             session.getTransaction().commit();
             System.out.println("Done");
-            System.out.println(employee);
+
         } finally {
+            session.close();
             factory.close();
         }
     }
